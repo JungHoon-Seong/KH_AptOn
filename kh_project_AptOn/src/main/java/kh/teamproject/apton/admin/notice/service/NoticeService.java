455e275ab85fc76738463206946ba1774cc60f09
kim9140 <kym3052@naver.com>
2021-11-16 17:54:04 +0900

    Notice.vo ,mapper 생성

---
+++ kh_project_AptOn/src/main/java/kh/teamproject/apton/admin/notice/service/NoticeService.java
@@ -0,0 +1,9 @@
+package kh.teamproject.apton.admin.notice.service;
+
+public class NoticeService {
+
+	public NoticeService() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ kh_project_AptOn/src/main/java/kh/teamproject/apton/admin/notice/vo/Notice.java
@@ -0,0 +1,73 @@
+package kh.teamproject.apton.admin.notice.vo;
+
+import java.sql.Date;
+
+public class Notice {
+
+	private int notice_num;
+	private String admin_id;
+	private String notice_title;
+	private String notice_content;
+	private String notice_image;
+	private Date notice_date;
+	
+	@Override
+	public String toString() {
+		return "Notice [notice_num=" + notice_num + ", admin_id=" + admin_id + ", notice_title=" + notice_title
+				+ ", notice_content=" + notice_content + ", notice_image=" + notice_image + ", notice_date="
+				+ notice_date + "]";
+	}
+
+	public int getNotice_num() {
+		return notice_num;
+	}
+
+	public void setNotice_num(int notice_num) {
+		this.notice_num = notice_num;
+	}
+
+	public String getAdmin_id() {
+		return admin_id;
+	}
+
+	public void setAdmin_id(String admin_id) {
+		this.admin_id = admin_id;
+	}
+
+	public String getNotice_title() {
+		return notice_title;
+	}
+
+	public void setNotice_title(String notice_title) {
+		this.notice_title = notice_title;
+	}
+
+	public String getNotice_content() {
+		return notice_content;
+	}
+
+	public void setNotice_content(String notice_content) {
+		this.notice_content = notice_content;
+	}
+
+	public String getNotice_image() {
+		return notice_image;
+	}
+
+	public void setNotice_image(String notice_image) {
+		this.notice_image = notice_image;
+	}
+
+	public Date getNotice_date() {
+		return notice_date;
+	}
+
+	public void setNotice_date(Date notice_date) {
+		this.notice_date = notice_date;
+	}
+
+	public Notice() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
