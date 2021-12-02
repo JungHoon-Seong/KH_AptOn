60a77ef566f8c1ce45e3b0daa4c9b94e1bd43a21
junyebPark <wnsduq5694@gmail.com>
2021-12-02 11:39:47 +0900

    공동관리비 기능 구현 추가(작성중)

--- kh_project_AptOn/src/main/java/kh/teamproject/apton/common_energy_usage/model/dao/CommonUsageDao.java
+++ kh_project_AptOn/src/main/java/kh/teamproject/apton/common_energy_usage/model/dao/CommonUsageDao.java
@@ -1,24 +1,52 @@
 package kh.teamproject.apton.common_energy_usage.model.dao;
 
+ 
+
 import java.util.List;
 
+ 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+ 
+
 import kh.teamproject.apton.common_energy_usage.model.vo.CommonUsage;
 
+ 
+
 @Repository("commonUsageDao")
+
 public class CommonUsageDao {
+
 	
+
 	@Autowired
+
 	private SqlSession sqlSession;
+
+ 
 
 	public CommonUsage selectOne(int cost_num) {
 		return sqlSession.selectOne("ComUsage.selectOne", cost_num);
+
 	}
+
 	
+
 	public List<CommonUsage> allprice(String date) {
 		return sqlSession.selectList("ComUsage.allprice", date);
+
 	}
+
+	
+
+	public List<CommonUsage> price(int cost_num) {
+		System.out.println("요기 cost_num : "+cost_num );
+		List<CommonUsage> result = sqlSession.selectList("ComUsage.allprice", String.valueOf(cost_num));
+		System.out.println("여기 몇개 읽은겨 : "+ result.size());
+		return result;
+
+	}
+
 }
--- kh_project_AptOn/src/main/java/kh/teamproject/apton/common_energy_usage/model/service/CommonUsageService.java
+++ kh_project_AptOn/src/main/java/kh/teamproject/apton/common_energy_usage/model/service/CommonUsageService.java
@@ -1,25 +1,35 @@
 package kh.teamproject.apton.common_energy_usage.model.service;
 
+ 
+
 import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import kh.teamproject.apton.common_energy_usage.model.dao.CommonUsageDao;
 import kh.teamproject.apton.common_energy_usage.model.vo.CommonUsage;
 
+ 
+
 @Service("ComUsageService")
+
 public class CommonUsageService {
-	
+
 	@Autowired
 	private CommonUsageDao commonUsageDao;
 	
 	public CommonUsage selecet(int cost_num) {
 		return commonUsageDao.selectOne(cost_num);
 	}
-	
-	public List<CommonUsage> price(String date) {
-		return commonUsageDao.allprice(date);
+//	public List<CommonUsage> price(String date) {
+
+//		return commonUsageDao.allprice(date);
+
+//	}
+
+	public List<CommonUsage> price(int cost_num){
+		return commonUsageDao.price(cost_num);
 	}
 
 }
